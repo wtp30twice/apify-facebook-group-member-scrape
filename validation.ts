83dee088fa49b9fdfa6895f3f2a807e81e945112
wtp30twice <floweryfruitbasket@gmail.com>
2026-02-09 23:35:55 -0600

    Add files via upload

---
+++ validation.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+import { allActorInputValidationFields } from 'crawlee-one';
+import type { FbGroupMemberActorInput } from './config';
+
+const inputValidationSchema = Joi.object<FbGroupMemberActorInput>({
+  ...allActorInputValidationFields,
+  maxMembers: Joi.number().integer().min(0).allow(null).optional(),
+});
+
+export const validateInput = (input: FbGroupMemberActorInput | null) => {
+  Joi.assert(input, inputValidationSchema);
+};
